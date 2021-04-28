dc706e209449e0547aa9af562a28919021d44d10
arielfreelancer <ariel.aguilarit@gmail.com>
2021-04-27 20:32:17 -0400

    Dockerize App

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,7 +7,7 @@ import { CronModule } from './cron/cron.module';
 
 @Module({
   imports: [ 
-    MongooseModule.forRoot('mongodb://localhost/cron'), CronModule, 
+    MongooseModule.forRoot('mongodb://mongo/cron'), CronModule, 
     CronModule,
   ],
   controllers: [AppController],
